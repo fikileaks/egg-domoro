60b34a9269c0c42c8ce52e76866d0a544a99a9ea
Fiqky Mukti <fiqky.akbar@gmail.com>
2025-04-24 09:45:35 +0700

    add config

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -2,15 +2,20 @@ import { createBrowserRouter } from 'react-router-dom'
 import Home from '../pages/home/Home'
 import NotFound from '../pages/not-found/NotFound'
 
-const route = createBrowserRouter([
+const route = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '*',
+      element: <NotFound />,
+    },
+  ],
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '*',
-    element: <NotFound />,
-  },
-])
+    basename: '/egg-domoro', // Tambahkan basename sesuai dengan base di vite.config
+  }
+)
 
 export default route
