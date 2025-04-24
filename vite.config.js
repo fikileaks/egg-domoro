c5189dcfa65de1f48295f4dee012628bc595cc5c
Fiqky Mukti <fiqky.akbar@gmail.com>
2025-04-24 09:21:10 +0700

    add config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/egg-domoro',
+// })
+
 export default defineConfig({
   plugins: [react()],
   base: '/egg-domoro',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        notFound: 'index.html', // fallback buat GitHub Pages
+      },
+    },
+  },
 })
